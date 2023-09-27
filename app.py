a7cf5d46176479ada572061b19b7803740fdb276
omolevictor97 <omolevictor97@gmail.com>
2023-09-27 20:02:00 +0100

    app.py updated

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
-
+from housing.logger import logging as log
+from housing.exception import HousingException
+import sys
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return "Hello Machine Learners"
+    try:
+        log.info("We are testing logger package")
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        log.info("Error has occured at the index function of app.py")
+        housing = HousingException(e, sys)
+        log.info(housing.error_message)
 
 
 
