eaf23bee05f6ba3d995ceea6d927849f9289b572
omolevictor97 <omolevictor97@gmail.com>
2023-09-28 08:11:12 +0100

    __init__.py file updated with comment

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -4,6 +4,11 @@ import sys
 class HousingException(Exception):
 
     def __init__(self, error_message:Exception, error_detail:sys):
+
+        """
+        error_message: An Exception object
+        error_detail: A sys module object
+        """
         super().__init__(error_message)
         self.error_message = HousingException.get_detailed_error_message(
             error_message=error_message,
@@ -15,6 +20,8 @@ class HousingException(Exception):
     def get_detailed_error_message(error_message:Exception, error_detail:sys) -> str:
         
         _, _, exec_tb = error_detail.exc_info()
+        # The error_detail.exc_info() returns a tuple that contains:
+        # A type, value and traceback object
         line_number = exec_tb.tb_frame.f_lineno
         file_name = exec_tb.tb_frame.f_code.co_filename
         error_message = f"""
